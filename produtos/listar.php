be97f02618f0336df6da1198511ca53a267e4ed6
Maria Eduarda <m64810816@gmail.com>
2026-08-27 11:52:02 -0300

    Aula_05_Listagem_Produtos  e css

--- produtos/listar.php
+++ produtos/listar.php
@@ -1,9 +1,46 @@
-<?php include '../verifica_login.php'; ?>
-<?php include '../cabecalho.php'; ?>
-<?php include '../css/style.css';?>
+<?php
+require __DIR__ . '/../conexao.php';
+require __DIR__ . '/verifica_login.php';
+require __DIR__ . '/../cabecalho.php';
+$sql = "SELECT * FROM produtos";
+$resultado = mysqli_query($conexao, $sql);
+
+
+
+?>
+
 <main>
-    <p>Bem-vindo(a), <?php echo $_SESSION['usuario_nome']; ?>!</p>
-    <!-- conteúdo da página -->
+    <h2 class="tprodutos ">Produtos cadastrados</h2>
+    <a href="cadastrar.php"><button type="submit" class="btslistrar">Cadastrar novo produto</button></a>
+
+    <table>
+        <tr>
+            <th>Produto</th>
+            <th>Descrição</th>
+            <th>Preço</th>
+            <th>Qtd.</th>
+            <th>Ações</th>
+        </tr>
+
+    <?php while ($produto = mysqli_fetch_assoc($resultado)) { ?>
+
+    <tr>
+        <td><?php echo $produto['nome']; ?></td>
+        <td><?php echo $produto['descricao']; ?></td>
+        <td>R$ <?php echo $produto['preco']; ?></td>
+        <td><?php echo $produto['quantidade']; ?></td>
+        <td>
+            <a href="atualizar.php?id=<?php echo $produto['id'];
+        ?>"><button type="submit" class="btslistrar">Editar</button></a>
+            <a href="excluir.php?id=<?php echo $produto['id'];
+        ?>"><button type="submit" class="btslistrar">Excluir</button></a>
+                    </td>
+                </tr>
+                <?php } ?>
+    </table>
 </main>
 
-<?php include '../rodape.php'; ?>
+
+<?php
+require __DIR__ . '/../rodape.php';
+?>
