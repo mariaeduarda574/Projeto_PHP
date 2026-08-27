66cb14a6a632b535c82ff4ecc68c7ab2696e6d0a
Maria Eduarda <m64810816@gmail.com>
2026-08-27 11:49:37 -0300

    Aula_05_Listagem_Produtos e css

--- conexao.php
+++ conexao.php
@@ -1,17 +1,16 @@
 <?php
-    //Conexão.php
+    // conexao.php
     $host = "localhost";
     $usuario = "root";
     $senha = "";
     $banco = "sistema_produtos";
     $conexao = mysqli_connect($host, $usuario, $senha, $banco);
 
-    //  if ($conexao) {
+    //var_dump($conexao);
+
+    // if ($conexao) {
     //     echo "Conexão realizada com sucesso!";
     // } else {
     //     echo "Erro ao conectar: " . mysqli_connect_error();
     // }
-
-
-
 ?>
--- login.php
+++ login.php
@@ -1,30 +1,27 @@
- 
 <?php
 session_start();
-include 'conexao.php';
+require __DIR__ . '/conexao.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    $sql = "SELECT * FROM usuarios WHERE email = '$email' AND senha ='$senha'";
-
+    $sql = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
     $resultado = mysqli_query($conexao, $sql);
 
     if (mysqli_num_rows($resultado) == 1) {
         $usuario = mysqli_fetch_assoc($resultado);
-
         $_SESSION['usuario_id'] = $usuario['id'];
         $_SESSION['usuario_nome'] = $usuario['nome'];
-    header("Location: produtos/listar.php");
+        header('Location: /projeto_php/produtos/listar.php');
         exit;
     } else {
-        $mensagem = "E-mail ou senha inválidos.";
+        $mensagem = 'E-mail ou senha inválidos.';
     }
 }
 ?>
 
-<?php include 'cabecalho.php';?>
+<?php require __DIR__ . '/cabecalho.php'; ?>
 
 <main>
     <h2>Login</h2>
@@ -33,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <p><?php echo $mensagem; ?></p>
     <?php } ?>
 
-    <form action="login.php" method="POST">
+    <form action="/projeto_php/login.php" method="POST">
         <label>E-mail:</label>
         <input type="text" name="email"><br>
 
@@ -42,9 +39,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         <button type="submit">Entrar</button>
     </form>
-
 </main>
 
-<?php include 'rodape.php';?>
-
-
+<?php require __DIR__ . '/rodape.php'; ?>
