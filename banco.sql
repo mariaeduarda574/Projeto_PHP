87f1577c0f49fc7f827efd289fda50eb31da0b80
Maria Eduarda <m64810816@gmail.com>
2026-09-17 11:15:14 -0300

    Aula 8 — Remoção de Produtos

--- banco.sql
+++ banco.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10-Set-2026 às 16:44
--- Versão do servidor: 10.4.22-MariaDB
--- versão do PHP: 8.1.2
+-- Tempo de geração: 17/09/2026 às 16:11
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,7 +26,7 @@ USE `sistema_produtos`;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `produtos`
+-- Estrutura para tabela `produtos`
 --
 
 CREATE TABLE `produtos` (
@@ -35,24 +35,23 @@ CREATE TABLE `produtos` (
   `descricao` varchar(255) NOT NULL,
   `preco` decimal(10,2) NOT NULL,
   `quantidade` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `produtos`
+-- Despejando dados para a tabela `produtos`
 --
 
 INSERT INTO `produtos` (`id`, `nome`, `descricao`, `preco`, `quantidade`) VALUES
-(4, ' Lacreme Ao Leite', 'Tablete Lacreme Ao Leite Cacau Show', '22.00', 8),
-(5, 'LaCreme de Mezzo ', 'Tablete LaCreme de Chocolate Mezzo 100g', '19.99', 10),
-(6, 'laCreme Branco ', 'Tablete laCreme de Chocolate Branco 100g', '20.00', 3),
-(7, 'laCreme ao Leite e Avelã', 'Tablete laCreme de Chocolate ao Leite e Avelã 100g', '22.00', 27),
-(8, 'Tablete Te Amo', 'Tablete ao Leite Chocoarte Te Amo 40g', '16.99', 3),
-(9, 'LaCreme Gold de Chocolate Branco', 'Tablete laCreme Gold de Chocolate Branco 100g ', '21.99', 6);
+(4, ' Lacreme Ao Leite', 'Tablete Lacreme Ao Leite Cacau Show', 22.00, 8),
+(5, 'LaCreme de Mezzo ', 'Tablete LaCreme de Chocolate Mezzo 100g', 19.99, 10),
+(6, 'laCreme Branco ', 'Tablete laCreme de Chocolate Branco 100g', 20.00, 3),
+(7, 'laCreme ao Leite e Avelã', 'Tablete laCreme de Chocolate ao Leite e Avelã 100g', 22.00, 27),
+(9, 'LaCreme Gold de Chocolate Branco', 'Tablete laCreme Gold de Chocolate Branco 100g ', 21.99, 6);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuarios`
+-- Estrutura para tabela `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -60,10 +59,10 @@ CREATE TABLE `usuarios` (
   `nome` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `senha` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `usuarios`
+-- Despejando dados para a tabela `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
@@ -75,26 +74,26 @@ INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`) VALUES
 --
 
 --
--- Índices para tabela `produtos`
+-- Índices de tabela `produtos`
 --
 ALTER TABLE `produtos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices para tabela `usuarios`
+-- Índices de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
